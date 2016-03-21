a2bd40f9faec985c2ae8a9e70cf4c7fdf1271c9c
lruckman <larry.ruckman@gmail.com>
2016-03-20 21:21:24 -0700

    Cleanup of constants

--- src/Web/Engine/Constants.cs
+++ src/Web/Engine/Constants.cs
@@ -2,13 +2,7 @@
 {
     public static class Constants
     {
-        public const string RelativeDocumentPath = "app_data\\documents";
         public const string ThumbnailExtension = ".thm";
-
-        public const string LuceneFieldOCR = "__contents__";
-        public const string LuceneFieldTitle = "__title__";
-        public const string LuceneFieldAbstract = "__abstract__";
-        public const string LuceneFieldId = "id";
 
         public const int SearchResultsPageSize = 25;
     }
