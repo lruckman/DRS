e489e1a0c7d29f9825c67415bc79a8f5f2b53788
lruckman <larry.ruckman@gmail.com>
2016-05-29 10:34:24 -0700

    AutoMapper and Mediator split into separate Registries.

---
+++ src/Web/Engine/CommandHandling/MediatorRegistry.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using StructureMap;
+
+namespace Web.Engine.CommandHandling
+{
+    public class MediatorRegistry : Registry
+    {
+        public MediatorRegistry()
+        {
+            Scan(scanner =>
+            {
+                scanner.AssemblyContainingType<IMediator>();
+                scanner.AssemblyContainingType<MediatorRegistry>();
+                scanner.WithDefaultConventions();
+
+                // MediatR
+
+                scanner.AssemblyContainingType<IMediator>();
+                scanner.ConnectImplementationsToTypesClosing(typeof (IRequestHandler<,>));
+                scanner.ConnectImplementationsToTypesClosing(typeof (IAsyncRequestHandler<,>));
+                scanner.ConnectImplementationsToTypesClosing(typeof (INotificationHandler<>));
+                scanner.ConnectImplementationsToTypesClosing(typeof (IAsyncNotificationHandler<>));
+            });
+
+            // MediatR
+
+            For<SingleInstanceFactory>()
+                .Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>()
+                .Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+        }
+    }
+}
--- src/Web/Engine/WebRegistry.cs
+++ src/Web/Engine/WebRegistry.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using AutoMapper;
-using MediatR;
-using StructureMap;
+﻿using StructureMap;
 using StructureMap.Graph;
 
 namespace Web.Engine
@@ -14,59 +9,11 @@ namespace Web.Engine
         {
             Scan(scanner =>
             {
-                scanner.Assembly(Assembly.GetExecutingAssembly());
                 scanner.TheCallingAssembly();
                 scanner.WithDefaultConventions();
-                
-                // AutoMapper
-
-                scanner.AddAllTypesOf<Profile>();
-
-                // MediatR
-
-                scanner.AssemblyContainingType<IMediator>();
-                scanner.ConnectImplementationsToTypesClosing(typeof (IRequestHandler<,>));
-                scanner.ConnectImplementationsToTypesClosing(typeof (IAsyncRequestHandler<,>));
-                scanner.ConnectImplementationsToTypesClosing(typeof (INotificationHandler<>));
-                scanner.ConnectImplementationsToTypesClosing(typeof (IAsyncNotificationHandler<>));
+                scanner.LookForRegistries();
+                scanner.AssemblyContainingType<WebRegistry>();
             });
-
-            // MediatR
-
-            For<SingleInstanceFactory>()
-                .Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
-            For<MultiInstanceFactory>()
-                .Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
-
-            // AutoMapper
-
-            var profiles =
-                typeof (WebRegistry).Assembly.GetTypes()
-                    .Where(t => typeof (Profile).IsAssignableFrom(t))
-                    .Select(t => (Profile) Activator.CreateInstance(t));
-
-            var config = new MapperConfiguration(cfg =>
-            {
-                foreach (var profile in profiles)
-                {
-                    cfg.AddProfile(profile);
-                }
-            });
-
-            config.AssertConfigurationIsValid(); // ¯\_(ツ)_/¯
-
-            For<MapperConfiguration>()
-                .Singleton()
-                .Use(config);
-
-            For<IConfigurationProvider>()
-                .Singleton()
-                .Use(config);
-
-            For<IMapper>()
-                .Singleton()
-                .Use(ctx => ctx.GetInstance<MapperConfiguration>()
-                    .CreateMapper(ctx.GetInstance));
         }
     }
 }
