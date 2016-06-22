82f2e5c6afb6adc1c2bde7a708509ba6b4099cc5
lruckman <larry.ruckman@gmail.com>
2016-06-21 20:49:52 -0700

    Adding handler tests

---
+++ test/WebTests/ViewModels/Api/Documents/GetTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Web.Models;
+using Web.ViewModels.Api.Documents;
+using Xunit;
+
+namespace WebTests.ViewModels.Api.Documents
+{
+    public class GetTests
+    {
+        [Fact]
+        public async Task QueryHandler_ReturnsCorrectModelType()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseInMemoryDatabase();
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            var db = new ApplicationDbContext(optionsBuilder.Options);
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<Get.QueryHandler.MappingProfile>());
+            var queryHandler = new Get.QueryHandler(db, config);
+            var query = new Get.Query {Id = 1};
+            var result = await queryHandler.Handle(query);
+
+            Assert.IsType<Get.Result>(result);
+        }
+    }
+}
