c540442e78366337d14963132daa4a5624d7201d
lruckman <larry.ruckman@gmail.com>
2016-08-07 01:30:10 -0700

    Moved common functionality into new services.

---
+++ src/Web/Engine/Services/FileEncryptor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Web.Engine.Extensions;
+
+namespace Web.Engine.Services
+{
+    public interface IFileEncryptor
+    {
+        byte[] Encrypt(byte[] buffer, byte[] key);
+        byte[] Decrypt(Stream stream, byte[] key = null);
+        byte[] Decrypt(byte[] buffer, byte[] key = null);
+        byte[] Decrypt(string s, byte[] key = null);
+        byte[] DecryptBase64(string s, byte[] key = null);
+        byte[] DecryptFile(string path, byte[] key = null);
+    }
+
+    public class FileEncryptor : IFileEncryptor
+    {
+        private const DataProtectionScope DataProtectionScope =
+            System.Security.Cryptography.DataProtectionScope.LocalMachine;
+
+        /// <summary>
+        ///     Encrypts the provided byte[]. The byte[] passed in will have its reference updated.
+        /// </summary>
+        /// <returns>Returns the encrypted byte[]</returns>
+        public byte[] Encrypt(byte[] buffer, byte[] key)
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            return buffer = ProtectedData.Protect(buffer, key, DataProtectionScope);
+        }
+
+        public byte[] Decrypt(Stream stream, byte[] key = null)
+        {
+            return Decrypt(stream.ToByteArray(), key);
+        }
+
+        public byte[] Decrypt(byte[] buffer, byte[] key = null)
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            return ProtectedData.Unprotect(buffer, key, DataProtectionScope);
+        }
+
+        public byte[] Decrypt(string s, byte[] key = null)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            return Decrypt(Encoding.UTF8.GetBytes(s), key);
+        }
+
+        public byte[] DecryptBase64(string s, byte[] key = null)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            return Decrypt(Convert.FromBase64String(s), key);
+        }
+
+        public byte[] DecryptFile(string path, byte[] key = null)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException("Path does not exist.", nameof(path));
+            }
+
+            return Decrypt(File.ReadAllBytes(path), key);
+        }
+    }
+}
---
+++ src/Web/Engine/Services/FileMover.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Web.Models;
+using File = System.IO.File;
+
+namespace Web.Engine.Services
+{
+    public interface IFileMover
+    {
+        Task<string> Move(byte[] buffer, byte[] fileKey);
+    }
+
+    public class FileMover : IFileMover
+    {
+        private readonly DRSConfig _config;
+        private readonly ApplicationDbContext _db;
+        private readonly IFileEncryptor _encryptor;
+
+        public FileMover(IOptions<DRSConfig> config, ApplicationDbContext db, IFileEncryptor encryptor)
+        {
+            _config = config.Value;
+            _encryptor = encryptor;
+            _db = db;
+        }
+
+        public async Task<string> Move(byte[] buffer, byte[] fileKey)
+        {
+            var path = await GetNewFileName(_config.DocumentPath);
+
+            buffer = _encryptor.Encrypt(buffer, fileKey);
+
+            using (var fileStream = File.Create(path))
+            {
+                await fileStream.WriteAsync(buffer, 0, buffer.Length);
+            }
+
+            return path;
+        }
+
+        private async Task<string> GetNewFileName(string rootPath)
+        {
+            var seed = await _db.Documents.MaxAsync(d => d.Id);
+
+            var subFolder1 = Math.Ceiling((seed + 1) / 1024m / 1024m / 1024m);
+            var subFolder2 = Math.Ceiling(subFolder1 / 1024m / 1024m);
+            var subFolder3 = Math.Ceiling(subFolder2 / 1024m);
+
+            return Path.Combine(rootPath,
+                $@"{subFolder1}\{subFolder2}\{subFolder3}\{Guid.NewGuid():N}.bin");
+        }
+    }
+}
