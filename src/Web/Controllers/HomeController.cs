c3cf4b0106be0764ef8113ace132ce5586a033a6
lruckman <larry.ruckman@gmail.com>
2017-07-16 15:59:17 -0700

    redirect for better experience from default page. At some point the spa will be available on the default route.

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// TODO: investigate why this doesn't work in the features folder
+    /// </summary>
+    public class HomeController : BaseController
+    {
+        public IActionResult Index() => Redirect("~/documents");
+    }
+}
