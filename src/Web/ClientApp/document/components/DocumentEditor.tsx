0c08ec4ed5dc2939d57b7a818ac1b312529ccd32
lruckman <larry.ruckman@gmail.com>
2017-07-23 15:02:59 -0700

    DocumentEditor now uses BaseModal

--- src/Web/ClientApp/document/components/DocumentEditor.tsx
+++ src/Web/ClientApp/document/components/DocumentEditor.tsx
@@ -1,5 +1,6 @@
 ﻿import * as React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { BaseModal } from '../../components';
 import { DocumentThumbnail } from '../components';
 import { DocumentFile } from '../../models';
 
@@ -57,86 +58,73 @@ class DocumentEditor extends React.Component<OwnProps, OwnState> {
     }
 
     public render() {
-        return <Modal
-            backdrop="static"
-            bsSize="lg"
-            show={this.props.show}
-            onHide={this.props.onHide}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>
-                    Update Document
-                      </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <form className="form-horizontal">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <DocumentThumbnail
-                                alt={this.state.document.title}
-                                url={this.state.document.thumbnailLink}
-                            />
-                            <dl className="text-muted dl-horizontal">
-                                <dt>Pages:</dt>
-                                <dd>{this.state.document.pageCount}</dd>
-                                <dt>File Size:</dt>
-                                <dd>{(this.state.document.size / 1024).toFixed(2)} KB</dd>
-                            </dl>
-                        </div>
-                        <div className="col-md-8">
-                            {/*<ValidationSummary
+
+        const buttons = [
+            <Button key="closeButton" onClick={this.props.onHide}>Close</Button>
+            , <Button key="saveButton" onClick={this.handleSave} bsStyle="primary">Save Changes</Button>
+        ]
+
+        const documentForm = <form className="form-horizontal">
+            <div className="row">
+                <div className="col-md-4">
+                    <DocumentThumbnail
+                        alt={this.state.document.title}
+                        url={this.state.document.thumbnailLink}
+                    />
+                    <dl className="text-muted dl-horizontal">
+                        <dt>Pages:</dt>
+                        <dd>{this.state.document.pageCount}</dd>
+                        <dt>File Size:</dt>
+                        <dd>{(this.state.document.size / 1024).toFixed(2)} KB</dd>
+                    </dl>
+                </div>
+                <div className="col-md-8">
+                    {/*<ValidationSummary
                                     errors={this.state.validationErrors || undefined} />*/}
-                            <div className="form-group">
-                                <label htmlFor="title" className="col-sm-2 control-label">Title</label>
-                                <div className="col-sm-10">
-                                    <input
-                                        type="text"
-                                        defaultValue={this.state.document.title}
-                                        name="title"
-                                        className="form-control"
-                                        placeholder="Title"
-                                        autoFocus
-                                        onChange={this.handleChange}
-                                    />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="abstract" className="col-sm-2 control-label">Abstract</label>
-                                <div className="col-sm-10">
-                                    <textarea
-                                        className="form-control"
-                                        rows={Number("14")}
-                                        placeholder="Abstract"
-                                        name="abstract"
-                                        defaultValue={this.state.document.abstract}
-                                        onChange={this.handleChange}
-                                    />
-                                </div>
-                            </div>
-                            {/*<div className="form-group">
-                                    <label htmlFor="libraries" className="col-sm-2 control-label">Libraries</label>
-                                    <div className="col-sm-10">
-                                        <Select
-                                            multi
-                                            placeholder="Libraries"
-                                            simpleValue
-                                            value={this.state.document.libraryIds}
-                                            valueKey="value"
-                                            labelKey="text"
-                                            options={this.props.libraries}
-                                            onChange={this.handleLibraryChange}
-                                            />
-                                    </div>
-                                </div>*/}
+                    <div className="form-group">
+                        <label htmlFor="title" className="col-sm-2 control-label">Title</label>
+                        <div className="col-sm-10">
+                            <input
+                                type="text"
+                                defaultValue={this.state.document.title}
+                                name="title"
+                                className="form-control"
+                                placeholder="Title"
+                                autoFocus
+                                onChange={this.handleChange}
+                            />
                         </div>
                     </div>
-                </form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={this.props.onHide}>Close</Button>
-                <Button onClick={this.handleSave} bsStyle="primary">Save Changes</Button>
-            </Modal.Footer>
-        </Modal>
+                    <div className="form-group">
+                        <label htmlFor="abstract" className="col-sm-2 control-label">Abstract</label>
+                        <div className="col-sm-10">
+                            <textarea
+                                className="form-control"
+                                rows={Number("14")}
+                                placeholder="Abstract"
+                                name="abstract"
+                                defaultValue={this.state.document.abstract}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>;
+
+        const modalProps = {
+            buttons
+            , onHide: this.props.onHide
+            , show: this.props.show
+            , title: 'Update Document'
+            , className: 'modal-lg'
+            , closeButton: true
+            , children: documentForm
+        }
+
+        return <BaseModal
+            {...modalProps}
+        />;
     }
 }
 
